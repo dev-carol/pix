6aade0f096f625ee79d32c59000246116fb274e1
carol santos <contato.caroline1999@gmail.com>
2022-08-03 00:55:38 -0300

    config: webpack

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 
   const config = new DocumentBuilder()
   .setTitle('Api Pix')
@@ -13,5 +19,8 @@ async function bootstrap() {
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
   await app.listen(3000);
+  
 }
+
+
 bootstrap();
